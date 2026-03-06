3af18deaa8e2f81e19df0c06a7c85664d6375a8d
alion1992 <55086590+alion1992@users.noreply.github.com>
2026-03-06 12:17:44 +0100

    cambio ip

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseUrl: "https://restcountries.com/v3.1",
-    anilloUrl: "http://192.168.100.6:8082/api"
+    anilloUrl: "http://cifpvdgnas.myqnapcloud.com:8082/api"
 }
